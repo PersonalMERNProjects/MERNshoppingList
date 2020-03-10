dc73db69d366d5ef19c78c442da937f4b13d5b9f
Emmanuel Nnajiofor Chinonye <emiklad1993@gmail.com>
2020-03-10 13:36:42 +0100

    corrected my .gitignore in client folder to include the build folder

---
+++ client/build/precache-manifest.94d3a69d0e18f1540efb1786a869472d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4cc8e0f1dd1ddf47044931e16b2eec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "837fbd968255b0b6481a",
+    "url": "/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "ecbf30163224869e8fd1",
+    "url": "/static/css/main.d1b2e949.chunk.css"
+  },
+  {
+    "revision": "837fbd968255b0b6481a",
+    "url": "/static/js/2.478ad200.chunk.js"
+  },
+  {
+    "revision": "ecbf30163224869e8fd1",
+    "url": "/static/js/main.07427118.chunk.js"
+  },
+  {
+    "revision": "d4fe51dee9a6d17e674e",
+    "url": "/static/js/runtime-main.e3b45bf6.js"
+  }
+]);
